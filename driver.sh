91e3b1a36f573eef6af3a25a2a0d0685959b7617
albortas <151683964+albortas@users.noreply.github.com>
2025-02-14 08:29:50 -0400

    Create driver.sh

---
+++ driver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yay -S nvidia-390xx-dkms nvidia-390xx-settings nvidia-390xx-utils lib32-nvidia-390xx-utils opencl-nvidia-390xx lib32-opencl-nvidia-390xx vdpauinfo clinfo
+sudo nvidia-xconfig
